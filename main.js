c50e552673732cdbd5e861ff5779f84ceb3bb45f
Irina Sinitsyna <hauteballe@gmail.com>
2020-12-10 23:03:16 +0300

    feat: implement flex panel gallery

---
+++ main.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  panels.forEach((panel) => panel.classList.remove("open"));
+  this.classList.toggle("open");
+}
+
+function toggleActive(event) {
+  if (event.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
